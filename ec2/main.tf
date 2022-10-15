83d55a0e1179f61279de00912c3ec6afe13d3d54
nasser amer <nasser@nassers-MacBook-Pro.local>
2022-10-15 17:52:07 -0400

    lastest

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,3 +4,20 @@ resource "aws_vpc" "imtiaz-vpc" {
     Name = "imtiaz-test-vpc1"
   }
 }
+
+resource "aws_subnet" "in_secondary_cidr" {
+  vpc_id     = aws_vpc.imtiaz-vpc.id
+  cidr_block = "10.2.1.0/24"
+  tags = {
+    Name = "private-subnet-terraform"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id_nas
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
---
+++ ec2/variable.tf
@@ -0,0 +1,3 @@
+variable "ami_id_nas" {
+    default = "ami-026b57f3c383c2eec"  
+}
